96cecebcb97394733da96eff4285fe38071d1720
Joseph Garner <joseph.peter.garner@gmail.com>
2022-01-09 13:22:53 +1100

    feat(#3): added landing page and basic text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,73 +1,35 @@
 import Head from 'next/head';
+import 'animate.css';
 import Image from 'next/image';
 
 import styles from '@/styles/Home.module.css';
+import { getDateOfWeek } from '@/util/dateUtil';
 
 export default function Home() {
+  const dayIs = getDateOfWeek();
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>TypeScript starter for Next.js</title>
-        <meta
-          name="description"
-          content="TypeScript starter for Next.js that includes all you need to build amazing apps"
-        />
+        <title>Day by Day</title>
+        <meta name="description" content="Simple life planner" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{` `}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <p className={styles.description}>This is not an official starter!</p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=typescript-nextjs-starter"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+      <main
+        className={`animate__animated animate__fadeIn animate__delay ${styles.main}`}
+      >
+        <h1 className={styles.title}>Welcome.</h1>
+        <h1 className={styles.title}>Today is {dayIs}.</h1>
       </main>
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=typescript-nextjs-starter"
+          href="https://www.gnu.org/licenses/gpl-3.0.en.html"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{` `}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
+          Licensed under the GPL-3.0 License
         </a>
       </footer>
     </div>
---
+++ src/util/dateUtil.tsx
@@ -0,0 +1,21 @@
+export function getDateOfWeek(): string {
+  const now = new Date();
+
+  switch (now.getDay()) {
+    case 0:
+      return `Sunday`;
+    case 1:
+      return `Monday`;
+    case 2:
+      return `Tuesday`;
+    case 3:
+      return `Wednesday`;
+    case 4:
+      return `Thursday`;
+    case 5:
+      return `Friday`;
+    case 6:
+      return `Saturday`;
+  }
+  return ``;
+}
